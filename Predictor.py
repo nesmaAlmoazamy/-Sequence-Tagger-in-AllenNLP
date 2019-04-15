d86feaadece5b198290c64eb2f1855fbe43416a3
Nesma Almoazamy <nesma.almoazamy93@gmail.com>
2019-04-15 23:50:25 +0300

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -1,14 +1,35 @@
-from overrides import overrides
-
-from allennlp.common.util import JsonDict
+from typing import Iterator, List, Dict
+import torch
+import torch.optim as optim
+import numpy as np
 from allennlp.data import Instance
-from allennlp.predictors.predictor import Predictor
+from allennlp.data.fields import TextField,LabelField,SequenceLabelField
+from allennlp.data.dataset_readers import DatasetReader
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.models import Model
+from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
+from allennlp.modules.token_embedders import Embedding
+from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder, PytorchSeq2SeqWrapper
+from allennlp.modules.seq2vec_encoders import Seq2VecEncoder, PytorchSeq2VecWrapper
+from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
+from allennlp.training.metrics import CategoricalAccuracy
+from allennlp.data.iterators import BucketIterator
+from allennlp.training.trainer import Trainer
+from allennlp.predictors import SentenceTaggerPredictor
+from allennlp.data.tokenizers import Token, CharacterTokenizer
 
+from io import open
+import glob
+import os
+import unicodedata
+import string
 
 @Predictor.register('name-Predictor')
 class Names_Predictor(Predictor):
 
-    @overrides
+  
     def predict_json(self, json_dict: JsonDict) -> JsonDict:
         name = json_dict['name']
         instance = self._dataset_reader.text_to_instance(name=name)
@@ -18,7 +39,7 @@ class Names_Predictor(Predictor):
 
         return {"instance": self.predict_instance(instance), "all_labels": all_labels}
 
-    @overrides
+    
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
         name = json_dict['name']
         return self._dataset_reader.text_to_instance(name=name)
